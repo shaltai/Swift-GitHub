0253546f765d9a8f3a76d8a307403b4005c2f57f
Stas Pimenov <yarsklad@gmail.com>
2021-12-23 20:33:22 +0300

    Commit cell constraints fixes.

--- GitHub/Views/Cells/CommitTableViewCell.swift
+++ GitHub/Views/Cells/CommitTableViewCell.swift
@@ -13,6 +13,12 @@ class CommitTableViewCell: UITableViewCell {
       setup()
    }
    
+   override func layoutSubviews() {
+      super.layoutSubviews()
+      
+      contentView.frame = contentView.frame.inset(by: UIEdgeInsets(top: 0, left: 16, bottom: 8, right: 16))
+   }
+   
    func initCell(commit: Commits.Items) {
       fullNameLabel.text = commit.repository.full_name
       messageLabel.text = commit.commit.message
@@ -20,33 +26,51 @@ class CommitTableViewCell: UITableViewCell {
       let formatter = DateFormatter()
       formatter.dateFormat = "E, d MMM"
       commitDateLabel.text = " committed \(formatter.string(from: commit.commit.committer.commitDate))"
-//      commitDateLabel.text = "commited Date"
       shaLabel.text = String(commit.sha.prefix(6))
    }
    
    func setup() {
       // settings
-      backgroundColor = .systemGray6
+      contentView.backgroundColor = .systemGray6
+      contentView.layer.cornerRadius = 8
+      shaLabel.setContentCompressionResistancePriority(UILayoutPriority(rawValue: 760), for: .horizontal)
+      shaLabel.setContentHuggingPriority(UILayoutPriority(rawValue: 260), for: .horizontal)
       // subviews
-      for view in [fullNameLabel, messageLabel, committerLabel, commitDateLabel, shaLabel] {
-         addSubview(view)
+      let view = UIView()
+      contentView.addSubview(view)
+      contentView.addSubview(shaLabel)
+      for label in [fullNameLabel, messageLabel, committerLabel, commitDateLabel] {
+         label.numberOfLines = 0
+         label.lineBreakMode = .byCharWrapping
+         view.addSubview(label)
       }
       // constraints
-      fullNameLabel.setupEdgeConstraints(top: topAnchor,
-                                         leading: leadingAnchor,
-                                         padding: UIEdgeInsets(top: 16, left: 16, bottom: 8, right: 16))
+      view.setupEdgeConstraints(top: contentView.topAnchor,
+                                trailing: shaLabel.leadingAnchor,
+                                bottom: contentView.bottomAnchor,
+                                leading: contentView.leadingAnchor,
+                                padding: UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 8))
+      fullNameLabel.setupEdgeConstraints(top: view.topAnchor,
+                                         trailing: view.trailingAnchor,
+                                         leading: view.leadingAnchor,
+                                         padding: UIEdgeInsets(top: 0, left: 0, bottom: 16, right: 0))
       messageLabel.setupEdgeConstraints(top: fullNameLabel.bottomAnchor,
-                                        leading: leadingAnchor,
-                                        padding: UIEdgeInsets(top: 0, left: 16, bottom: 8, right: 16))
+                                        trailing: view.trailingAnchor,
+                                        leading: view.leadingAnchor,
+                                        padding: UIEdgeInsets(top: 0, left: 0, bottom: 16, right: 0))
       committerLabel.setupEdgeConstraints(top: messageLabel.bottomAnchor,
-                                          leading: leadingAnchor,
-                                          padding: UIEdgeInsets(top: 0, left: 16, bottom: 24, right: 16))
-      commitDateLabel.setupEdgeConstraints(top: messageLabel.bottomAnchor,
-                                          leading: committerLabel.trailingAnchor,
-                                          padding: UIEdgeInsets(top: 0, left: 0, bottom: 24, right: 16))
-      shaLabel.setupEdgeConstraints(top: topAnchor,
-                                    trailing: trailingAnchor,
-                                    padding: UIEdgeInsets(top: 16, left: 0, bottom: 8, right: 16))
+                                          bottom: view.bottomAnchor,
+                                          leading: view.leadingAnchor,
+                                          padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 0))
+      commitDateLabel.setupEdgeConstraints(top: committerLabel.topAnchor,
+                                           trailing: view.trailingAnchor,
+                                           bottom: committerLabel.bottomAnchor,
+                                           leading: committerLabel.trailingAnchor)
+//      shaLabel.firstBaselineAnchor.constraint(equalTo: fullNameLabel.firstBaselineAnchor).isActive = true
+      shaLabel.setupEdgeConstraints(top: fullNameLabel.topAnchor,
+                                    trailing: contentView.trailingAnchor,
+                                    bottom: fullNameLabel.bottomAnchor,
+                                    padding: UIEdgeInsets(top: 0, left: 0, bottom: 0, right: 16))
    }
    
    override func setSelected(_ selected: Bool, animated: Bool) {
